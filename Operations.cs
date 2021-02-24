633590c8c3996efd4e9eb427483c0bd6a44448ae
DamonFlick <75501983+DamonFlick@users.noreply.github.com>
2021-02-24 11:20:50 -0800

    Add project files.

---
+++ Operations.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_5
+{
+    static class Operations
+    {
+        public static void divide(int x)
+        {
+            int result = x / 2;
+            Console.WriteLine(result);
+        }
+
+        
+        public static void divide(int x, int y, out int z)
+        {
+             z = x / y;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to be divided by two: ");
+           Operations.divide(Convert.ToInt32(Console.ReadLine()));
+
+            Console.WriteLine("Now enter two numbers. The second number is the divisor: "); 
+            Operations.divide(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()),out int z);
+            Console.WriteLine(z);
+            Console.ReadLine();
+        }
+    }
+}
